2ac11346b138f7a1817705625109db3d26f3e955
DuyNguyen <duynkce150247@fpt.edu.vn>
2022-06-16 11:18:40 +0700

    add back end project source

---
+++ thegioididong_group1_mockproject/src/main/java/com/group1/thegioididong/ThegioididongGroup1MockprojectApplication.java
@@ -0,0 +1,13 @@
+package com.group1.thegioididong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThegioididongGroup1MockprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThegioididongGroup1MockprojectApplication.class, args);
+    }
+
+}
